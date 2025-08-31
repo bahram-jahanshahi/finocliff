4e2b60724cbbf688d2f8f561484d0b7a2dadcfcf
Bahram Jahanshahi <bahram.jahanshahi@gmail.com>
2025-08-31 10:52:36 +0200

    add finocliff service

---
+++ finocliff-service/src/main/java/se/bahram/finance/finocliff_service/FinocliffServiceApplication.java
@@ -0,0 +1,13 @@
+package se.bahram.finance.finocliff_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinocliffServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinocliffServiceApplication.class, args);
+	}
+
+}
---
+++ finocliff-service/src/test/java/se/bahram/finance/finocliff_service/FinocliffServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.bahram.finance.finocliff_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinocliffServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
